02f4868b79950cb48db7df3ba4433dbc3f315990
SermusAnton <antonsermus@mail.ru>
2018-07-11 21:16:04 +0300

    First TDD

---
+++ Simple/GoodByer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple {
+    public class GoodByer {
+        private string who;
+        public string Message {
+            get { return "Привет "+who; }
+            set { who = value; }
+        }
+        public GoodByer() {
+            who = "Anonim";
+        }
+        public GoodByer(string who) {
+            if (who == "") {
+                throw new Exception("No person to say goodbay!!!");
+            }
+            this.who = who;
+        }
+    }
+}
---
+++ TestSimple/TestGoodByer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple;
+using NUnit.Framework;
+
+namespace TestSimple
+{
+    [TestFixture]
+    public class TestGoodByer
+    {
+        [Test()]
+        public void TestConstructorWithParams() {
+            Assert.Throws<Exception>( delegate(){ GoodByer goodByer = new GoodByer(""); });
+        }
+    }
+}
