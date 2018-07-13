68c5eae7a7a567aa101a588d8b7bf5c5b5c787b4
SermusAnton <antonsermus@mail.ru>
2018-07-13 23:30:45 +0300

    Add find

---
+++ Simple/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple {
+    /// <summary>
+    /// http://codekata.com/kata/kata02-karate-chop/
+    /// </summary>
+    public class BinarySearch {
+        public int Find(int findValue, int[] sortedArray) {
+            if (sortedArray == null || sortedArray.Length == 0) { 
+                return -1;
+            }
+            if (sortedArray[0] == findValue) {
+                    return 0;
+            }
+            return Bisect(findValue,0,sortedArray.Length-1,sortedArray);
+        }
+        public int Bisect(int findValue, int startInd, int endInd, int[] sortedArray) {
+            if (startInd >= endInd) {
+                return -1;
+            }
+            int middleInd = (startInd + endInd) / 2;
+            if (findValue < sortedArray[middleInd]) {
+                return Bisect(findValue, startInd, middleInd, sortedArray);
+            }
+            if (findValue > sortedArray[middleInd]) {
+                return Bisect(findValue, middleInd, endInd, sortedArray);
+            }
+            if (findValue == sortedArray[middleInd]) {
+                return middleInd;
+            }
+            return -1;
+        }
+    }
+}
---
+++ TestSimple/TestBinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple;
+using NUnit.Framework;
+
+namespace TestSimple
+{
+    [TestFixture]
+    public class TestBinarySearch
+    {
+        #region Группа 1
+        [Test()]
+        public void LengthNullElement() {
+            BinarySearch binarySearch = new BinarySearch();
+            Assert.AreEqual(-1, binarySearch.Find(3, new int[]{ }));
+           
+        }
+        [Test()]
+        public void LengthOneElement() {
+            BinarySearch binarySearch = new BinarySearch();
+            Assert.AreEqual(-1, binarySearch.Find(3, new int[] { 1 }));
+        }
+        [Test()]
+        public void FindInOneElement() {
+            BinarySearch binarySearch = new BinarySearch();
+            Assert.AreEqual(0, binarySearch.Find(1, new int[] { 1 }));
+        }
+        #endregion
+        #region Группа 2
+        [Test()]
+        public void FindIfFistIndex() {
+            BinarySearch binarySearch = new BinarySearch();
+            Assert.AreEqual(0, binarySearch.Find(1, new int[] { 1,3,5 }));
+        }
+        [Test()]
+        public void FindIndex() {
+            BinarySearch binarySearch = new BinarySearch();
+            Assert.AreEqual(1, binarySearch.Find(3, new int[] { 1, 3, 5 }));
+        }
+        #endregion
+    }
+}
